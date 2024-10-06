c64844e116eac7507cbde038742da0738af7f8b5
Nangil <surasit.dang@bumail.net>
2024-10-06 19:20:48 +0700

    CameraControll

---
+++ Assets/CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Transform target;
+    Vector3 velocity = Vector3.zero;
+
+    [Range(0f, 1f)]
+    public float smoothTime;
+
+    public Vector3 positionOffset;
+
+    [Header("Axis Llimitation")]
+    public Vector2 xLimit;
+    public Vector2 yLimit;
+
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position + positionOffset;
+        targetPosition = new Vector3(Mathf.Clamp(targetPosition.x, xLimit.x, xLimit.y), Mathf.Clamp(targetPosition.y, yLimit.x, yLimit.y), -10);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
