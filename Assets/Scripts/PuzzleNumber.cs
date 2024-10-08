fbffd2f60c958c2af0686d5df0838fe3efccda04
NetsuTo <narawichsonwai1z@gmail.com>
2024-10-09 01:34:14 +0700

    Puzzle_2

--- Assets/Scripts/PuzzleNumber.cs
+++ Assets/Scripts/PuzzleNumber.cs
@@ -12,9 +12,17 @@ public class PuzzleNumber : MonoBehaviour
     [SerializeField] private TextMeshProUGUI Ans;
 
     private string Answer = "0910";
+    private bool isPlayerInRange = false; // ตัวแปรสถานะผู้เล่นอยู่ในระยะ
 
     private void Update()
     {
+        // ตรวจสอบการกดปุ่มเมื่อผู้เล่นอยู่ในพื้นที่
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E) && !puzzlePanel.activeSelf)
+        {
+            OpenPuzzle();
+        }
+
+        // ตรวจสอบการปิด Puzzle ด้วยปุ่ม Escape
         if (puzzlePanel.activeSelf)
         {
             if (Input.GetKeyDown(KeyCode.Escape))
@@ -23,6 +31,7 @@ public class PuzzleNumber : MonoBehaviour
             }
         }
     }
+
     public void Number(int num)
     {
         Ans.text += num.ToString();
@@ -30,15 +39,14 @@ public class PuzzleNumber : MonoBehaviour
 
     public void Check()
     {
-        if(Ans.text == Answer)
+        if (Ans.text == Answer)
         {
             //play sound
             ClosePuzzle();
         }
         else
         {
-            Ans.text = "";
-            
+            Ans.text = "Error";
         }
     }
 
@@ -53,14 +61,19 @@ public class PuzzleNumber : MonoBehaviour
         objectToDestroy.SetActive(false);
     }
 
-     void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (Input.GetKeyDown(KeyCode.E) && !puzzlePanel.activeSelf)
-            {
-                OpenPuzzle();
-            }
+            isPlayerInRange = true; // เมื่อผู้เล่นเข้ามาในระยะ
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = false; // เมื่อผู้เล่นออกจากระยะ
         }
     }
 }
